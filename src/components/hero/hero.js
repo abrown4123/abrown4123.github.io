477d7577772f9ef1defb202df1507486987e296c
Anthony Brown <abrown4123@gmail.com>
2018-08-09 18:55:04 -0500

    Finished function that cycles through subheadings inside of hero component

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
 
-const description = ["Web Developer", "Nesquik Connoiseur", "Other Witty Thing"]
+class Hero extends Component {
 
-export default class Hero extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      subheading_index: 0
+    };
+  }
+
+  componentDidMount() {
+    this.cycleSubheading();
+  }
 
 //Eventually the code below is going to animate between the description array values
 
-  // renderDescription() {
-  //   return setTimeout(() => {
-  //       let i = 0;
-  //       i++;
-  //       return descriptions[i];
-  //     }, 1000);
-  // }
+  cycleSubheading() {
+    const self = this;
+    const time = setInterval(function () {
+      if (self.state.subheading_index > 0) {
+        clearInterval(time);
+      }
+      self.setState({subheading_index: self.state.subheading_index+1});
+    }, 1000);
+  };
 
   render() {
+    const description =  ["Web Developer", "Nesquik Connoiseur", "Other Witty Thing"]
+
     return (
       <div className="hero">
         <h1>Anthony Brown</h1>
-        <h2>{this.renderDescription()}</h2>
+        <h2>{description[this.state.subheading_index]}</h2>
       </div>
     );
   }
 }
+
+export default Hero;
