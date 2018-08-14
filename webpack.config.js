d8103af54d7dea7eae9f6e45342a8e861427ca78
Anthony Brown <abrown4123@gmail.com>
2018-08-14 09:15:00 -0500

    Configure webpack to allow css to be directly imported into components

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,8 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude:/node_modules/,
         use: ['babel-loader']
-      }
+      },
+      { test: /\.css$/, loader: "style-loader!css-loader" }
     ]
   },
   resolve: {
