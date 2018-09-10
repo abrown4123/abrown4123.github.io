e027794653d3f2bfa62631e10ac707e4bb8fcdc2
Anthony Brown <abrown4123@gmail.com>
2018-09-10 14:39:02 -0500

    Update project descriptions in Project section

--- src/reducers/reducer_projects.js
+++ src/reducers/reducer_projects.js
@@ -2,7 +2,7 @@ export default function() {
   return [
     {
       title: 'NachoCritique',
-      detail:"This is a fullstack application that uses allows users to add and comment on their favorite nachos.",
+      detail:"Nacho Critique is a full stack app that utilizes NodeJS, Express, MongoDB and Passport to allow users create Nacho Critique accounts to rate Nachos. Users have the ability to log in and share their opinions of Nachos from different restaurants and edit reviews they've previously made. Users can also add pictures and their own critiques of Nachos they've tried.",
       tech: 'Node, Express, MongoDB, EJS',
       image: '../images/nacho.png',
       alt: 'Picture of a Nacho Rating App',
@@ -11,25 +11,25 @@ export default function() {
     },
     {
       title: 'Wikipedia Viewer',
-      detail:'This Views Wikipedia',
+      detail:'Wikipedia Viewer utilizes the MediaWiki API to return the top ten encyclopedia article\'s associated with the user\'s query. It also has a random article feature which retrieves a random article from the database and takes the user to that page.',
       image: '../images/Wiki.png',
       alt: 'Picture of a Wikipedia Search webpage',
-      tech: '',
+      tech: 'HTML, CSS, Vanilla JS, MediaWiki API',
       livelink: '#',
       codelink: '#'
     },
     {
       title: 'Weather App',
-      detail:'This shows the weather',
+      detail:'The Weather App uses the Open Weather Map API to display the user\'s local forecast. Users are able to toggle between Celsius and Fahrenheit display of the temperature and this App\'s background changes to reflect the user\'s forecast',
       image: '../images/Weather.png',
       alt: 'Picture of an App that displays weather conditions',
-      tech: '',
+      tech: 'HTML, CSS, Vanilla JS, Open Weather API',
       livelink: '#',
       codelink: '#'
     },
     {
       title: 'This Website',
-      detail:'This is my porfolio page. Feel free to use the code button below to see its code.',
+      detail:'This is my porfolio page. Feel free to use the code button below to see the code used behind the scenes.',
       image: '../images/portfolio.png',
       alt: 'Picture of this website',
       tech: 'React, Redux, Webpack, ES6, Babel',
